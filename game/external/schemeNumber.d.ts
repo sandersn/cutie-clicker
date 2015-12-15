78e41a133a020b2ae499894860309303fa112a78
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-15 14:55:25 -0800

    Improve schemeNumber types

--- game/external/schemeNumber.d.ts
+++ game/external/schemeNumber.d.ts
@@ -3,8 +3,10 @@
 // Documentation: http://john-edwin-tobey.org/Scheme/javascript-bignum/docs/files/schemeNumber-js.html
 // Definitions by: Nathan Shively-Sanders <nathansa@microsoft.com>, 
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
-// SchemeNumber
 type SchemeOperator = (...args: (string | SchemeNumber | number)[]) => SchemeNumber;
+declare var VERSION: number[];
+declare function raise(conditionType: string, message: string, ...irritants: any[]): void;
+declare var maxIntegerDigits: number;
 declare interface SchemeFn {
 	[fnname: string]: SchemeOperator;
 	floor: SchemeOperator;
@@ -15,7 +17,10 @@ declare interface SchemeFn {
 }
 declare interface SchemeNumber {
 	(value: string | number): SchemeNumber;
-	toFixed(n: number): number;
+	toString(radix: number);
+	toFixed(fractionDigits: number): string;
+	toExponential(fractionDigits: number): string;
+	toPrecision(precision: number): string;
 	add(n: number): void;
 	fn: SchemeFn;
 }
